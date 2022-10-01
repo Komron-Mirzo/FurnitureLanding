f954f0cac9a8201ceeff2466fccd0142346c6230
Komron-Mirzo <kabduvaliyev@gmail.com>
2022-10-01 13:43:56 +0500

    Testimonial Slider Fixed

---
+++ js/owl-script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    nav: true,
+    navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    responsive: {
+      0: {
+        items: 1,
+    
+      },
+      600: {
+     
+        items: 2
+      },
+      1000: {
+    
+        items: 3
+      }
+    }
+  });
+});
